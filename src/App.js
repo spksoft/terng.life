b3f88352148486f73573fe4d589540eabf019d79
Sarin Udompanit <sarin_udo@sendit.asia>
2018-06-30 01:48:37 +0700

    Remove not essential div

--- src/App.js
+++ src/App.js
@@ -7,18 +7,13 @@ import { hot } from 'react-hot-loader'
 import Routes from 'react-static-routes'
 
 // @todo #1:30min setup global css
-css.global('html, body', tw('font-demo-serif font-light text-base m-0 p-0'))
+css.global('html, body', tw('font-demo-serif font-light text-base m-0 p-0 min-h-full '))
 css.global('a', tw('no-underline text-demo-color font-bold'))
-const Content = glamorous.div(tw('p-4'))
 
 // @todo #1:30min add rematch store
 const App = () => (
   <Router>
-    <div>
-      <Content>
-        <Routes />
-      </Content>
-    </div>
+    <Routes />
   </Router>
 )
 
