1727af1fd008420b771c67d060bc7e427e982299
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2018-07-02 11:37:10 +0700

    Raplacing lorem , add button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import glamorous from 'glamorous'
+import { css } from 'glamor'
 
 const PageContainer = glamorous.div(tw('flex flex-wrap h-screen'))
 const LeftContainer = glamorous.section(tw('w-full sm:w-full md:w-1/2 lg:w-3/7 self-center text-center '))
 const RightContainer = glamorous.section(tw('w-full sm:w-full md:w-1/2 lg:w-4/7 flex self-center justify-center md:justify-start mb-1'))
 const ContentContainer = glamorous.section(tw('flex flex-col w-4\/5 justify-center overflow-y-scroll max-h-screen'))
 const Content = glamorous.p(tw('h-full'))
+const B = glamorous.b({
+  fontWeight: 'bold',
+})
+const Button = glamorous.button(tw('bg-white hover:bg-gray text-black font-bold py-2 px-4 rounded'))
+
+const buttonGroupCSS = css({
+  display: 'flex',
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '50px 0px 0px 0px',
+})
 
 export default () => (
   <PageContainer>
@@ -13,68 +26,16 @@ export default () => (
       <h1>Sippakorn Raksakiart</h1>
     </LeftContainer>
     <RightContainer>
-      <ContentContainer style = {{ 'maxHeight': '500px' }}>
+      <ContentContainer style={{ maxHeight: '500px' }}>
         <Content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          risus nunc, ultrices tincidunt finibus quis, cursus et eros. Curabitur
-          rutrum, felis at fringilla venenatis, felis lacus posuere mauris, vitae
-          tincidunt elit est ac ante. Phasellus sit amet odio tempus urna maximus
-          finibus. Vestibulum aliquam nibh dignissim orci condimentum elementum.
-          Ut ante nisl, scelerisque eget fermentum nec, luctus vel diam.
-          Etiam sed feugiat arcu, et dignissim diam. Integer eros sapien,
-          dictum a scelerisque ut, vestibulum sed ante. Nunc eleifend arcu libero,
-          vel semper odio dignissim eu. Etiam vel imperdiet nisi. Nullam congue
-          fringilla cursus. Nullam commodo fermentum elit eget interdum.
-          Fusce consequat molestie turpis et porta. Praesent ligula massa,
-          tincidunt in tempus eget, fermentum non massa. Nunc aliquam quam vitae
-          orci pellentesque rutrum. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Pellentesque euismod quam dolor,
-          sed suscipit massa blandit eget. Nam a accumsan arcu.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          risus nunc, ultrices tincidunt finibus quis, cursus et eros. Curabitur
-          rutrum, felis at fringilla venenatis, felis lacus posuere mauris, vitae
-          tincidunt elit est ac ante. Phasellus sit amet odio tempus urna maximus
-          finibus. Vestibulum aliquam nibh dignissim orci condimentum elementum.
-          Ut ante nisl, scelerisque eget fermentum nec, luctus vel diam.
-          Etiam sed feugiat arcu, et dignissim diam. Integer eros sapien,
-          dictum a scelerisque ut, vestibulum sed ante. Nunc eleifend arcu libero,
-          vel semper odio dignissim eu. Etiam vel imperdiet nisi. Nullam congue
-          fringilla cursus. Nullam commodo fermentum elit eget interdum.
-          Fusce consequat molestie turpis et porta. Praesent ligula massa,
-          tincidunt in tempus eget, fermentum non massa. Nunc aliquam quam vitae
-          orci pellentesque rutrum. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Pellentesque euismod quam dolor,
-          sed suscipit massa blandit eget. Nam a accumsan arcu.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          risus nunc, ultrices tincidunt finibus quis, cursus et eros. Curabitur
-          rutrum, felis at fringilla venenatis, felis lacus posuere mauris, vitae
-          tincidunt elit est ac ante. Phasellus sit amet odio tempus urna maximus
-          finibus. Vestibulum aliquam nibh dignissim orci condimentum elementum.
-          Ut ante nisl, scelerisque eget fermentum nec, luctus vel diam.
-          Etiam sed feugiat arcu, et dignissim diam. Integer eros sapien,
-          dictum a scelerisque ut, vestibulum sed ante. Nunc eleifend arcu libero,
-          vel semper odio dignissim eu. Etiam vel imperdiet nisi. Nullam congue
-          fringilla cursus. Nullam commodo fermentum elit eget interdum.
-          Fusce consequat molestie turpis et porta. Praesent ligula massa,
-          tincidunt in tempus eget, fermentum non massa. Nunc aliquam quam vitae
-          orci pellentesque rutrum. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Pellentesque euismod quam dolor,
-          sed suscipit massa blandit eget. Nam a accumsan arcu.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          risus nunc, ultrices tincidunt finibus quis, cursus et eros. Curabitur
-          rutrum, felis at fringilla venenatis, felis lacus posuere mauris, vitae
-          tincidunt elit est ac ante. Phasellus sit amet odio tempus urna maximus
-          finibus. Vestibulum aliquam nibh dignissim orci condimentum elementum.
-          Ut ante nisl, scelerisque eget fermentum nec, luctus vel diam.
-          Etiam sed feugiat arcu, et dignissim diam. Integer eros sapien,
-          dictum a scelerisque ut, vestibulum sed ante. Nunc eleifend arcu libero,
-          vel semper odio dignissim eu. Etiam vel imperdiet nisi. Nullam congue
-          fringilla cursus. Nullam commodo fermentum elit eget interdum.
-          Fusce consequat molestie turpis et porta. Praesent ligula massa,
-          tincidunt in tempus eget, fermentum non massa. Nunc aliquam quam vitae
-          orci pellentesque rutrum. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Pellentesque euismod quam dolor,
-          sed suscipit massa blandit eget. Nam a accumsan arcu.
+          <div>
+            Hello, I'm <B>Terng</B>. Now, I'm a <B>Software Architect at True corporation (Thailand)</B>. I was born in Phitsanulok town and <B>I love to coding since I'm 13 years old</B>
+            when I was 15 year old my program <B>'sPkAutorunkiller' was publish in COMPUTER.TODAY magazine</B> and also I'm a owner of <B>sPkSecurityGen Pullo ChoiceChecker terng.life BikerApp</B> too.
+            I graduated from <B>Computer Engineering</B> department from <B>King Mongkut's University of Technology Thonburi</B>. For my work experince, <B>I'm Software Enginner at BlueBik, Contractor software enginner at HBOT.io, Full Stack Developer at FIN$TREET and Software Architect at True corporation</B>
+          </div>
+          <div {...buttonGroupCSS}>
+            <Button onClick={() => { window.open('https://www.linkedin.com/in/sippakorn-raksakiart-4b5365b9/') }}>Hire me if you can</Button>
+          </div>
         </Content>
       </ContentContainer>
     </RightContainer>
