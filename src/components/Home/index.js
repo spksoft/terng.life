1589888e2f76b43cc1e1543f5b1268ea76c664ae
spksoft NA <spkrsk.37@gmail.com>
2019-04-11 15:59:55 +0700

    add article form medium

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,9 @@ import glamorous from 'glamorous'
 import { css } from 'glamor'
 
 const PageContainer = glamorous.div(tw('flex flex-wrap h-screen'))
-const ContentContainer = glamorous.section(tw('flex flex-col justify-center items-center overflow-y-scroll max-h-screen'))
+const ContentContainer = glamorous.section(
+  tw('flex flex-col justify-center items-center overflow-y-scroll max-h-screen')
+)
 const Content = glamorous.div(tw('h-full'))
 const B = glamorous.b({
   fontWeight: 'bold',
@@ -27,36 +29,85 @@ const paddingLeftRight = css({
   padding: '0px 3px 0px 3px',
 })
 
-export default () => (
-  <PageContainer>
-    <ContentContainer style={{ margin: '0px auto 0px auto' }}>
-      <Content>
-        <div style={{ margin: '50px 0px 0px 0px' }}>
-          <ContentContainer>
-            <img height="100" width="100" style={{ borderRadius: '50%' }} alt="Avatar" src="img/profile.jpg" />
-          </ContentContainer>
-          <ContentContainer>
-            <h1>Sippakorn Raksakiart</h1>
-          </ContentContainer>
-          <div style={{ margin: '0px auto 0px auto', padding: '0px 50px 0px 50px', maxWidth: '500px', minWidth: '100px' }}>
-          Hello, My name is Sippakorn Raksakiart (Terng). Now, I'm a Software Architect at TrueCorp. I learn programming since I was 13 year olds and I'm a owner of sPkAutorunkiller. It's a autorun killer program that was publish to COMPUTER.TODAY magazine. I'm the first developer at FINSTREET.co. If you wanna hire me let click 'Hire me if you can' button below.
-          </div>
-          <div {...buttonGroupCSS}>
-            <Button style={{ cursor: 'pointer' }} onClick={() => { window.open('https://www.linkedin.com/in/sippakorn-raksakiart-4b5365b9/') }}>Hire me if you can</Button>
-          </div>
-          <div {...buttonGroupCSS} {...iconButton}>
-            <div {...paddingLeftRight}>
-              <a href="https://github.com/spksoft" target="_blank"><img width={16} height={16} src="img/github-logo.png" /></a>
+export default ({ mediumFeedData }) => {
+  const { items } = mediumFeedData
+  console.log(items)
+  return (
+    <PageContainer>
+      <ContentContainer style={{ margin: '0px auto 0px auto' }}>
+        <Content>
+          <div style={{ margin: '50px 0px 0px 0px' }}>
+            <ContentContainer>
+              <img
+                height="100"
+                width="100"
+                style={{ borderRadius: '50%' }}
+                alt="Avatar"
+                src="img/profile.jpg"
+              />
+            </ContentContainer>
+            <ContentContainer>
+              <h1>Sippakorn Raksakiart</h1>
+            </ContentContainer>
+            <div
+              style={{
+                margin: '0px auto 0px auto',
+                padding: '0px 50px 0px 50px',
+                maxWidth: '500px',
+                minWidth: '100px',
+              }}
+            >
+              Hello, My name is Sippakorn Raksakiart (Terng). Now, I'm a Software Architect at
+              TrueCorp. I learn programming since I was 13 year olds and I'm a owner of
+              sPkAutorunkiller. It's a autorun killer program that was publish to COMPUTER.TODAY
+              magazine. I'm the first developer at FINSTREET.co. If you wanna hire me let click
+              'Hire me if you can' button below.
             </div>
-            <div {...paddingLeftRight}>
-              <a href="https://medium.com/@sippakornraksakiart" target="_blank"><img width={16} height={16} src="img/medium-size.png" /></a>
+            <ContentContainer>
+              <h1>Articles</h1>
+              {items.map(item => (
+                <a target="_blank" href={item.link}>
+                  {item.title}
+                </a>
+              ))}
+              <a
+                style={{ marginTop: '20px' }}
+                target="_blank"
+                href="https://medium.com/@sippakornraksakiart"
+              >
+                More...
+              </a>
+            </ContentContainer>
+            <div {...buttonGroupCSS}>
+              <Button
+                style={{ cursor: 'pointer' }}
+                onClick={() => {
+                  window.open('https://www.linkedin.com/in/sippakorn-raksakiart-4b5365b9/')
+                }}
+              >
+                Hire me if you can
+              </Button>
             </div>
-            <div {...paddingLeftRight}>
-              <a href="mailto:spkrsk.37@gmail.com"><img width={16} height={16} src="img/opened-email-envelope.png" /></a>
+            <div {...buttonGroupCSS} {...iconButton}>
+              <div {...paddingLeftRight}>
+                <a href="https://github.com/spksoft" target="_blank">
+                  <img width={16} height={16} src="img/github-logo.png" />
+                </a>
+              </div>
+              <div {...paddingLeftRight}>
+                <a href="https://medium.com/@sippakornraksakiart" target="_blank">
+                  <img width={16} height={16} src="img/medium-size.png" />
+                </a>
+              </div>
+              <div {...paddingLeftRight}>
+                <a href="mailto:spkrsk.37@gmail.com">
+                  <img width={16} height={16} src="img/opened-email-envelope.png" />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </Content>
-    </ContentContainer>
-  </PageContainer>
-)
+        </Content>
+      </ContentContainer>
+    </PageContainer>
+  )
+}
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
+import Parser from 'rss-parser'
 import HomeComponent from '../components/Home'
 
-export default withSiteData(() => (
-  <div>
-    <head>
-      <title>Terng life is all about my life !!!</title>
-    </head>
-    <HomeComponent />
-  </div>
-))
+const withMediumFeedData = Component =>
+  class extends React.Component {
+    state = {
+      loading: true,
+      data: undefined,
+    }
+    async componentDidMount () {
+      this.setState(() => ({
+        loading: true,
+      }))
+      const parser = new Parser()
+      const feed = await parser.parseURL(
+        'https://cors-anywhere.herokuapp.com/https://medium.com/feed/@sippakornraksakiart'
+      )
+      this.setState(() => ({
+        loading: false,
+        data: feed,
+      }))
+    }
+    render () {
+      const { loading, data } = this.state
+      if (loading) {
+        return <div>Loading</div>
+      } else if (!loading && !data) {
+        return <div>Something went wrong!</div>
+      }
+      return <Component mediumFeedData={data} {...this.props} />
+    }
+  }
+
+export default withSiteData(
+  withMediumFeedData(({ mediumFeedData }) => (
+    <div>
+      <head>
+        <title>Terng life is all about my life !!!</title>
+      </head>
+      <HomeComponent mediumFeedData={mediumFeedData} />
+    </div>
+  ))
+)
