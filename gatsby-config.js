1a7cedb6f2eeabf8e9fb16ae08a096646400948b
spksoft NA <spkrsk.37@gmail.com>
2019-09-11 12:49:20 +0700

    Add blog Cognitive Bias

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,13 +59,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `TERNG.LIFE Blog`,
+        short_name: `TERNG.LIFE`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/profile-pic.jpg`,
       },
     },
     `gatsby-plugin-offline`,
