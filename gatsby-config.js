67b3542abee4af9b02eef3a4b402d51004c7c637
Sippakorn Raksakiart <spkrsk.37@gmail.com>
2023-01-10 11:44:21 +0700

    update tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `UA-80654035-1`,
+        trackingId: `G-MLQWP1PW8P`,
       },
     },
     `gatsby-plugin-feed`,
